3ca2fc323e3eb77068debc513864ecd8699f94b1
Timochan <teemo@timochan.cn>
2022-06-29 16:15:39 +0800

    Update config.ts

--- config.ts
+++ config.ts
@@ -8,7 +8,7 @@ export const github = {
 
 export const mxSpace = {
   url: 'https://www.timochan.cn',
-  api: 'https://www.timochan.cn/apc',
+  api: 'https://api.timochan.cn/apc',
 }
 
 export const timeZone = 'Asia/Shanghai'
