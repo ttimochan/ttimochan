db97def8a0c5516da5d745816b62340848c88dee
timochan <i@timochan.cn>
2025-11-09 21:28:08 +0800

    Update config.ts

--- config.ts
+++ config.ts
@@ -9,7 +9,7 @@ export const opensource = {
     ],
   },
 };
-export const motto = `<p align=center><strong>お可愛いこと。</strong></p>`;
+export const motto = `<p align=center><strong>可爱</strong></p>`;
 export const github = {
   name: "ttimochan",
 };
